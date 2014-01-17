095111b20044a011fd2cbeca44e8b1d25834a3ca
Emanuel Aguirre <emakeka@gmail.com>
2014-01-17 17:19:07 -0200

    Add spanish translation

---
+++ messages/es/widget.php
@@ -0,0 +1,15 @@
+<?php
+return array(
+    '1#Add files|0#Choose file'       => '1#Añadir archivos|0#Elige archivo',
+    'File is too big'                 => 'Archivo muy grande',
+    'File is too small'               => 'Archivo muy pequeño',
+    'Filetype not allowed'            => 'Tipo de archivo no permitido',
+    'Max number of files exceeded'    => 'Excedió la cantidad de archivos',
+    'Uploaded bytes exceed file size' => 'Bytes cargados exceden el tamaño del archivo',
+    'Empty file upload result'        => 'Sin resultado de carga',
+    "Error"                           => 'Error',
+    "Start"                           => 'Iniciar',
+    "Cancel"                          => 'Cancelar',
+    "Delete"                          => 'Eliminar'
+
+);
