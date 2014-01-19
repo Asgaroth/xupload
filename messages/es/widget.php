3c5e401ca618180b9240e24f1b37df5e33fc7ee7
Emanuel Aguirre <emakeka@gmail.com>
2014-01-19 02:54:08 -0200

    Update widget.php

--- messages/es/widget.php
+++ messages/es/widget.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-    '1#Add files|0#Choose file'       => '1#Añadir archivos|0#Elige archivo',
-    'File is too big'                 => 'Archivo muy grande',
-    'File is too small'               => 'Archivo muy pequeño',
+    '1#Add files|0#Choose file'       => '1#Elegir archivos|0#Elige archivo',
+    'File is too big'                 => 'El archivo muy grande',
+    'File is too small'               => 'El archivo muy pequeño',
     'Filetype not allowed'            => 'Tipo de archivo no permitido',
-    'Max number of files exceeded'    => 'Excedió la cantidad de archivos',
+    'Max number of files exceeded'    => 'Excedió la cantidad maxima de archivos',
     'Uploaded bytes exceed file size' => 'Bytes cargados exceden el tamaño del archivo',
     'Empty file upload result'        => 'Sin resultado de carga',
     "Error"                           => 'Error',
